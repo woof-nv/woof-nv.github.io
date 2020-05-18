24ce126772bfff57f3e504927f108fc677fd641f
woof-nv <woof.nv@gmail.com>
2020-05-17 21:05:47 -0700

    js v.0

--- app.js
+++ app.js
@@ -2,35 +2,58 @@
   'use strict';
   var app = (function (doc) {
     var eventIs = false;
+
+    var closeAll = function () {
+      var eventWrap = doc.querySelectorAll('.event-wrap');
+      eventWrap.forEach(function (ele) {
+        ele.firstElementChild.classList.remove('active');
+      });
+      doc.querySelector('.about').classList.remove('aboutt');
+      doc.querySelectorAll('.message').forEach(function (span) {
+        span.classList.remove('foo');
+      });
+
+      eventIs = false;
+    };
+    
     var buttonToggle = function (eleId) {
       switch (eleId) {
         case 'popMore':
         case 'popAbout':
         case 'popContact':
-          if (eventIs && eventIs !== eleId) doc.querySelector('.' + eventIs).classList.remove('active');
+          if (eventIs && eventIs !== eleId) {
+            doc.querySelector('.' + eventIs).classList.remove('active');
+            doc.querySelector('.about').classList.remove('aboutt');
+            doc.querySelectorAll('.message').forEach(function (span) {
+              span.classList.remove('foo');
+            });
+          }
           if (!eventIs || true) {
             doc.querySelector('.' + eleId).classList.toggle('active');
-            // doc.querySelector('.' + eleId).classList.replace('active', 'hidden');
             console.log(eventIs, eleId);
             eventIs = eleId;
+          }
+          if (eleId === 'popMore') {
+            console.log(doc.querySelectorAll('.message'));
+            doc.querySelectorAll('.message').forEach(function(span){
+              span.classList.toggle('foo');
+            });
+          }
+          if(eleId === 'popAbout') {
+            console.log('about', eleId);
+            doc.querySelector('.about').classList.toggle('aboutt')
           }
           break;
         default:
             console.log(eleId);
           break;
       }
-    }
-    var closeAll = function () {
-      var eventWrap = doc.querySelectorAll('.event-wrap');
-      eventWrap.forEach(ele => ele.firstElementChild.classList.remove('active'));
-      eventIs = false;
-    }
+    };
 
     return function (ele) {
-      // console.log(ele);
       if (ele) buttonToggle(ele);
-      // if (ele === 'popMore') console.log('do additional animation');
-      // if (ele === 'popAbout') console.log('do additional animation');
+      if (ele === 'popMore') console.log('do additional animation');
+      if (ele === 'popAbout') console.log('do additional animation');
       if (ele === '') closeAll(ele);
     }
   })(document);
