16fb31db28fc70ab788e7ddcf7dba8e318c4174b
woof-nv <woof.nv@gmail.com>
2020-05-19 19:20:33 -0700

    more clean up

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 (function(){
   'use strict';
   var app = (function (doc) {
-    //used to keep track of app state; when false app has been reset
+    //eventIs is used to keep track of the app state; when false app has been reset.
     var eventIs = false;
-    //used to close all active animations
+    //toggleAnimations is used to close all active animations.
     var toggleAnimations = function () {
       doc.querySelector('.about').classList.remove('about-color');
       doc.querySelectorAll('.message').forEach(function (span) {
         span.classList.remove('show-message');
       });
     };
-    //used to reset the app's state
+    //closeAll is used to reset the app's state.
     var closeAll = function () {
       Object.values(doc.querySelector('.pop-up-container').children).forEach(function(ele){
         ele.classList.remove('active', 'inactive');
@@ -18,50 +18,50 @@
       toggleAnimations();
       eventIs = false;
     };
-    //controls button events
+    //controls button events.
     var buttonToggle = function (eleId) {
-      //cases are buttons
+      //cases are buttons.
       switch (eleId) {
         case 'popMore':
         case 'popAbout':
         case 'popContact':
           if (eventIs && eventIs !== eleId) {
-            //logic: not starting button event and user in event and clicks a different button
-            //true: previous button's event ends
+            //condition: user has clicked a button and user clicks a different botton.
+            //if true: previous button's event ends.
             doc.querySelector('.' + eventIs).classList.replace('active', 'inactive');
             toggleAnimations();
           }
           if (!eventIs || true) {
-            //logic: user's first button event or all button events excluding button event switch
+            //condition: user's first button click or all other button clicks excluding button switching.
             var element = doc.querySelector('.' + eleId);
-
+            //toggles button from active to inactive and vice versa.
             if (element.classList.contains('active')) {
-              //logic: element is active?
-              //true: element now inactive
+              //condition: element is active?
+              //if true: element now inactive
               element.classList.replace('active', 'inactive');
             } 
             else if (element.classList.contains('inactive')){
-              //logic: element is inactive?
-              //true: element now active
+              //condition: element is inactive?
+              //if true: element now active
               element.classList.replace('inactive', 'active');
             } 
             else {
-              //true: new button event starts
+              //if true: first button is clicked
               element.classList.add('active');
             }
-            //eventIs now true
+            //user is now in a running state of the app
             eventIs = eleId;
           }
           if (eleId === 'popMore') {
-            //logic: popMore button event
-            //true: start animation for popMore
+            //condition: popMore button event?
+            //if true: start animation for popMore
             doc.querySelectorAll('.message').forEach(function(firstLetter){
               firstLetter.classList.toggle('show-message');
             });
           }
           if(eleId === 'popAbout') {
-            //logic: popAbout button event
-            //true: start animation for popAbout
+            //condition: popAbout button event?
+            //if true: start animation for popAbout
             doc.querySelector('.about').classList.toggle('about-color');
           }
           break;
@@ -70,24 +70,21 @@
       }
     };
     return function (ele) {
-      //logic: if ele is a button
-      //true: toggle button
+      //condition: element clicked has an id (is a button).
+      //if true: toggle button.
       if (ele) buttonToggle(ele);
-      //logic: if ele is not a button
-      //true: end events
+      //condition: element clicked doesn't have an id (is not a button).
+      //if true: reset app.
       if (ele === '') closeAll(ele);
     };
   })(document);
 
-  document.addEventListener('click', function (e) { 
-    app(e.target.id); 
-    console.log(e.target);
-  }, false);
+  document.addEventListener('click', function (e) { app(e.target.id); }, false);
 
   //prevent other elements from bubbling up.
   document.querySelector('.pop-up-container').addEventListener('click', function(e){ e.stopPropagation();}, false);
-  
+
   Object.values(document.querySelector('.pop-up-container').children).forEach(function(popUp){
     popUp.addEventListener('click', function(e){ e.stopPropagation(); }, false);
-  })
+  });
 })();
